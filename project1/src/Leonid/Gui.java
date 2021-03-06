3759e99521d0b57fd0fca73e7b91f91bf6d9bc89
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-03-07 01:56:57 +0200

    впринципе, все збс. Ток костыль есть из за слоев. Можно пофиксить довольно легко

--- project1/src/Leonid/Gui.java
+++ project1/src/Leonid/Gui.java
@@ -4,34 +4,60 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+class MyFrameConfig extends JFrame {
+    public static final HashMap<String, Color> valuesColors = new HashMap<String, Color>() {{
+        put("black", new Color(0x0));
+        put("brown", new Color(0x8B4513));
+        put("red", new Color(0xD51414));
+        put("orange", new Color(0xFF8D00));
+        put("yellow", new Color(0xEAFF00));
+        put("green", new Color(0x00880A));
+        put("blue", new Color(0x0A48DA));
+        put("violet", new Color(0x9F0AF1));
+        put("gray", new Color(0x575757));
+        put("white", new Color(0xFFFFFF));
+        put("gold", new Color(0xD4AF37));
+        put("silver", new Color(0xB7B7B7));
+        put("none", new Color(0xc17b23));
+    }};
+
+    public static final String[] valuesString =
+            {"none", "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "gray", "white"};
+
+    MyFrameConfig() {
+        this.setTitle("Resistance Calculator");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(640, 400);
+        this.getContentPane().setBackground(new Color(0xEDFBC1));
+        ImageIcon icon = new ImageIcon("resistor.png");
+        this.setIconImage(icon.getImage());
+    }
+}
 
 class MyFrame extends MyFrameConfig implements ActionListener {
     JButton button;
+    JLabel textLabel, imageLabel;
+    JPanel panel1, panel2, panel3, panel4, panel5;
     JComboBox combobox1, combobox2, combobox3, combobox4, combobox5;
-    JLabel textLabel;
+
     MyFrame() {
         String[] test = {"BLACK", "BROWN", "VIOLET", "YELLOW"};
         button = new MyButton(450, 290);
-        combobox1 = new MyComboBox(70, 230, test);
-        combobox2 = new MyComboBox(170, 230, test);
-        combobox3 = new MyComboBox(270, 230, test);
-        combobox4 = new MyComboBox(370, 230, test);
-        combobox5 = new MyComboBox(470, 230, test);
-        JLabel imageLabel = new MyLabelImage(15, 15);
-        this.setLayout(null);
+        combobox1 = new MyComboBox(70, 230, valuesString);
+        combobox2 = new MyComboBox(170, 230, valuesString);
+        combobox3 = new MyComboBox(270, 230, valuesString);
+        combobox4 = new MyComboBox(370, 230, valuesString);
+        combobox5 = new MyComboBox(470, 230, valuesString);
+        imageLabel = new MyLabelImage(15, 15);
         textLabel = new MyLabelText(25, 290);
-        JPanel panel1 = new MyPanel(80, 25);
-        JPanel panel2 = new MyPanel(168, 25);
-        JPanel panel3 = new MyPanel(238, 25);
-        JPanel panel4 = new MyPanel(302, 25);
-        JPanel panel5 = new MyPanel(418, 25);
-
-        button.addActionListener(this);
-        combobox1.addActionListener(this);
-        combobox2.addActionListener(this);
-        combobox3.addActionListener(this);
-        combobox4.addActionListener(this);
-        combobox5.addActionListener(this);
+        panel1 = new MyPanel(80, 25);
+        panel2 = new MyPanel(168, 25);
+        panel3 = new MyPanel(238, 25);
+        panel4 = new MyPanel(302, 25);
+        panel5 = new MyPanel(418, 25);
 
         this.add(imageLabel);
         this.add(button);
@@ -47,45 +73,79 @@ class MyFrame extends MyFrameConfig implements ActionListener {
         this.add(panel4);
         this.add(panel5);
 
-        panel1.setBackground(Color.green);
+        button.addActionListener(this);
+        combobox1.addActionListener(this);
+        combobox2.addActionListener(this);
+        combobox3.addActionListener(this);
+        combobox4.addActionListener(this);
+        combobox5.addActionListener(this);
 
+        this.setLayout(null);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button) {
+        ImageIcon image = new ImageIcon("Six-Band-Resistor-Color-Code.png");
+
+        if (e.getSource() == button) {
             textLabel.setText("ZOPA");
         }
-        if(e.getSource()==combobox1){
+        if (e.getSource() == combobox1) {
             String x = (String) combobox1.getSelectedItem();
-            textLabel.setText(x);
+            Color color = valuesColors.get(x);
+            panel1.setBackground(color);
+            imageLabel.setIcon(image);
+
+        } else if (e.getSource() == combobox2) {
+            String x = (String) combobox2.getSelectedItem();
+            Color color = valuesColors.get(x);
+            panel2.setBackground(color);
+            imageLabel.setIcon(image);
+
+        } else if (e.getSource() == combobox3) {
+            String x = (String) combobox3.getSelectedItem();
+            Color color = valuesColors.get(x);
+            panel3.setBackground(color);
+            imageLabel.setIcon(image);
+
+        } else if (e.getSource() == combobox4) {
+            String x = (String) combobox4.getSelectedItem();
+            Color color = valuesColors.get(x);
+            panel4.setBackground(color);
+            imageLabel.setIcon(image);
+
+        } else if (e.getSource() == combobox5) {
+            String x = (String) combobox5.getSelectedItem();
+            Color color = valuesColors.get(x);
+            panel5.setBackground(color);
+            imageLabel.setIcon(image);
         }
     }
 }
 
-class MyLabelImage extends JLabel{
-    MyLabelImage(int posx, int posy){
+class MyLabelImage extends JLabel {
+    MyLabelImage(int posx, int posy) {
         ImageIcon image = new ImageIcon("Six-Band-Resistor-Color-Code.png");
         this.setIcon(image);
-        this.setHorizontalTextPosition(JLabel.CENTER); // set text LEFT< RIGHT < CNTER
-        this.setVerticalTextPosition(JLabel.BOTTOM); // top, cent, bottom
+        this.setHorizontalTextPosition(JLabel.CENTER);
+        this.setVerticalTextPosition(JLabel.BOTTOM);
         this.setVerticalAlignment(JLabel.TOP);
         this.setHorizontalAlignment(JLabel.LEFT);
-        this.setBounds(posx, posy , 594, 350);
+        this.setBounds(posx, posy, 594, 350);
     }
 }
 
-class MyLabelText extends JLabel{
-    MyLabelText(int posx, int posy){
+class MyLabelText extends JLabel {
+    MyLabelText(int posx, int posy) {
         this.setText("10_000_TEST_000 Ohm +/- N/A");
         this.setForeground(Color.black);
         this.setFont(new Font("Rubik", Font.PLAIN, 24));
-        this.setBounds(posx, posy , 500, 40);
+        this.setBounds(posx, posy, 500, 40);
     }
 }
 
-class MyButton extends JButton{
-    MyButton(int posx, int poxy){
+class MyButton extends JButton {
+    MyButton(int posx, int poxy) {
         this.setText("GET RES");
         this.setBackground(new Color(0xFAFFA9));
         this.setFont(new Font("Rubik", Font.PLAIN, 20));
@@ -93,21 +153,19 @@ class MyButton extends JButton{
     }
 }
 
-class MyComboBox extends JComboBox{
-    MyComboBox(int posx, int posy, String[] array){
-//        String[] test = {"BLACK", "BROWN", "VIOLET", "YELLOW"};
+class MyComboBox extends JComboBox {
+    MyComboBox(int posx, int posy, String[] array) {
         DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(array);
         this.setModel(model);
-//        this.addItem("55");
         this.setBounds(posx, posy, 95, 30);
         this.setBackground(new Color(0xFAFFA9));
         this.setFont(new Font("Rubik", Font.PLAIN, 16));
     }
 }
 
-class MyPanel extends JPanel{
-    MyPanel(int posx, int posy){
-        this.setBackground(Color.red);
+class MyPanel extends JPanel {
+    MyPanel(int posx, int posy) {
+        this.setBackground(new Color(0xc17b23));
         this.setBounds(posx, posy, 60, 180);
     }
 }
--- project1/src/Leonid/Resistor.java
+++ project1/src/Leonid/Resistor.java
@@ -1,6 +1,49 @@
 package Leonid;
 
-public class Resistor extends ResistorsConfig {
+import java.util.HashMap;
+
+abstract class ResistorsConfig {
+    public static final HashMap<String, Double> valuesRes = new HashMap<String, Double>() {{
+        put("black", 0.0);
+        put("brown", 1.0);
+        put("red", 2.0);
+        put("orange", 3.0);
+        put("yellow", 4.0);
+        put("green", 5.0);
+        put("blue", 6.0);
+        put("violet", 7.0);
+        put("gray", 8.0);
+        put("white", 9.0);
+    }};
+
+    public static final HashMap<String, Double> multipliersRes = new HashMap<String, Double>() {{
+        put("silver", 0.01);
+        put("gold", 0.1);
+        put("black", 1.0);
+        put("brown", 10.0);
+        put("red", 100.0);
+        put("orange", 1000.0);
+        put("yellow", 10_000.0);
+        put("green", 100_000.0);
+        put("blue", 1_000_000.0);
+        put("violet", 10_000_000.0);
+        put("gray", 100_000_000.0);
+        put("white", 1_000_000_000.0);
+    }};
+
+    public static final HashMap<String, Double> tolerancesRes = new HashMap<String, Double>() {{
+        put("silver", 10.0);
+        put("gold", 5.0);
+        put("black", 1.0);
+        put("brown", 2.0);
+        put("green", 0.5);
+        put("blue", 0.25);
+        put("violet", 0.1);
+        put("gray", 0.05);
+    }};
+}
+
+class Resistor extends ResistorsConfig {
     protected String line1, line2, line3, line4, line5;
     private final int lines;
 
@@ -31,13 +74,13 @@ public class Resistor extends ResistorsConfig {
     public double getResistance() {
         double resistance;
         if (lines == 3) {
-            resistance = (values.get(line1) * 10 + values.get(line2)) * multipliers.get(line3);
+            resistance = (valuesRes.get(line1) * 10 + valuesRes.get(line2)) * multipliersRes.get(line3);
             return resistance;
         } else if (lines == 4) {
-            resistance = (values.get(line1) * 10 + values.get(line2)) * multipliers.get(line3);
+            resistance = (valuesRes.get(line1) * 10 + valuesRes.get(line2)) * multipliersRes.get(line3);
             return resistance;
         } else if (lines == 5) {
-            resistance = (values.get(line1) * 100 + values.get(line2) * 10 + values.get(line3)) * multipliers.get(line4);
+            resistance = (valuesRes.get(line1) * 100 + valuesRes.get(line2) * 10 + valuesRes.get(line3)) * multipliersRes.get(line4);
             return resistance;
         }
         return 0.0;
@@ -46,10 +89,10 @@ public class Resistor extends ResistorsConfig {
     public String getTolerance() {
         String tolerance;
         if (lines == 4) {
-            tolerance = "+/- " + tolerances.get(line4) + "%";
+            tolerance = "+/- " + tolerancesRes.get(line4) + "%";
             return tolerance;
         } else if (lines == 5) {
-            tolerance = "+/- " + tolerances.get(line5) + "%";
+            tolerance = "+/- " + tolerancesRes.get(line5) + "%";
             return tolerance;
         }
         return "N/A";
