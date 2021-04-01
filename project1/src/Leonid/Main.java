7aeae0d4ac70652df9cfb839b839380ea4475105
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-04-01 21:39:26 +0300

    url

--- project1/src/Leonid/Main.java
+++ project1/src/Leonid/Main.java
@@ -1,8 +1,20 @@
 package Leonid;
 
+import java.awt.*;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
         MyFrame frame = new MyFrame();
         frame.setVisible(true);
+        openWebpage("https://sisu.ut.ee/elektroonika/28-takistite-v%C3%A4rvikoodid");
+    }
+
+    public static void openWebpage(String urlString) {
+        try {
+            Desktop.getDesktop().browse(new URL(urlString).toURI());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
